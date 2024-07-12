ebe3e0bdebbf18802a4fbc07a58f6a612f5581ec
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2024-07-12 13:58:13 -0700

    comments

--- binary-search.js
+++ binary-search.js
@@ -79,4 +79,37 @@ function binarySearch(arr, target) {
 //     console.log(Date.now() - startTime);
 
 
+// // Fill an array with 1 million integers
+// n = 1000000;
+// arr = [];
+// for (let i = 0 ; i < n ; i ++) {
+//   arr.push(i);
+// }
+// // console.log(arr);
+
+// // Pick 10 thousand random values to search for, from -n to n
+// valuesToSearch = [];
+// for (let i = 0 ; i < 10000 ; i++) {
+//   valuesToSearch.push(Math.floor(Math.random() * 2 * n) - n);
+// }
+// // console.log(valuesToSearch);
+
+// startTime = Date.now();
+// for (let i = 0 ; i < valuesToSearch.length ; i++) {
+//   arr.includes(valuesToSearch[i]);
+// }
+// endTime = Date.now();
+
+// console.log(`Linear Search: ${endTime - startTime}ms`); // Linear Search: 8093ms
+
+
+// startTime = Date.now();
+// for (let i = 0 ; i < valuesToSearch.length ; i++) {
+//   binarySearch(arr, valuesToSearch[i]);
+// }
+// endTime = Date.now();
+
+// console.log(`Binary Search: ${endTime - startTime}ms`);  // Binary Search: 8ms
+
+
 module.exports = [linearSearch, binarySearch];
